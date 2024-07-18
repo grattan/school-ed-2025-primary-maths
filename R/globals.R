bf34f861d6ca11af0539a0e33c94f5bce2751043
Nick <nick.j.parkinson@gmail.com>
2024-07-18 10:14:55 +1000

    Teacher attitudes

---
+++ R/globals.R
@@ -0,0 +1,18 @@
+# Sets up libraries and common R objects to be used across Quarto documents
+
+# Libraries
+library(tidyverse)
+library(grattantheme)
+library(qualtRics)
+library(knitr)
+library(knitr)
+library(kableExtra)
+library(sjlabelled)
+library(readxl)
+library(janitor)
+library(scales)
+library(strayr)
+library(glue)
+
+# Additional configuration
+options(knitr.kable.NA = '')
