84ad546dba545ef46918b2a2fc16cf465319381f
Nick <nick.j.parkinson@gmail.com>
2024-07-22 19:01:19 +1000

    Update with survey weighting

--- R/globals.R
+++ R/globals.R
@@ -14,8 +14,21 @@ library(scales)
 library(strayr)
 library(glue)
 library(ggdirectlabel)
+library(mice)
+library(survey)
+library(srvyr)
+library(rmarkdown)
 
 # Additional configuration
 options(knitr.kable.NA = '')
 
 source_caption <- "Source: Grattan Institute's 2024 survey on primary mathematics."
+
+# Useful functions
+# Creates an ordered factor
+fct_case_when <- function(...) {
+args <- as.list(match.call())
+levels <- sapply(args[-1], function(f) f[[3]])  # extract RHS of formula
+levels <- levels[!is.na(levels)]
+factor(dplyr::case_when(...), levels=levels)
+}
