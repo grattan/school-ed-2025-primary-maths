c3627c6196c3113e08cb84f2e92fc5b5b466a05b
Tashya Sathyajit <tsat0002@student.monash.edu>
2024-07-30 12:26:06 +1000

    adding packages to setup, removing percentile bar charts

--- R/00-setup.R
+++ R/00-setup.R
@@ -14,10 +14,20 @@ library(purrr)
 library(glue)
 library(grattantheme) # remotes::install_github("grattan/grattantheme")
 library(haven)
+library(dplyr)
+library(kableExtra)
+library(Rrepest)
+library(ggplot2)
+library(ggrepel)
+library(ggdist)
+library(distributional)
+library(plotly)
+library(ggtext)
 
 # specific ------
 library(ggtext)     # for chart labelling
-
+library(survey)
+library(intsvy)
 
 # common objects ---------------------------------------------------------------
 
