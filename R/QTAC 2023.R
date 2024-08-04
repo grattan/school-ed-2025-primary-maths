00f1489ae15de10ca17c75757197d4b9b5aadfc0
DanielPetrie <daniel.petrie@grattaninstitute.edu.au>
2024-08-04 22:48:17 +1000

    Redefine Working Directory, and use readxl

--- R/QTAC 2023.R
+++ R/QTAC 2023.R
@@ -21,11 +21,18 @@ library(grattantheme) # remotes::install_github("grattan/grattantheme")
 
 # specific ------
 library(ggtext)     # for chart labelling
+library(readxl)
 
 # common objects ---------------------------------------------------------------
 
+  #Change working directory (currently set to Documents file in my OneDrive)
+  getwd()
+  setwd("C:/Users/PETRIE/OneDrive - The University of Melbourne/Documents/GitHub/school-ed-2025-primary-maths/")
+  getwd()
+
   #Define the data file with relative address, then read in and make it a tibble
   datafile2023 <- "data/QTAC-2023-admissions_RPS-8566.xlsx"
-  data2023 <- read.csv(datafile2023)
+  data2023 <- read_xlsx(datafile2023)
 
 # helper functions =============================================================
+
